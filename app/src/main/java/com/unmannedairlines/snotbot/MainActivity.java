e93dcce878fc85990aa8f621a5267ff1a390cbde
ChrisZ <czadra@whale.org>
2018-11-14 12:50:59 -0500

    Setting up branch for creating custom data logs

--- app/src/main/java/com/unmannedairlines/snotbot/MainActivity.java
+++ app/src/main/java/com/unmannedairlines/snotbot/MainActivity.java
@@ -13,7 +13,10 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -67,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
     // SDK version
     public TextView sdkVersion;
 
+    //for custom data logs
+    Button btnRead , btnSave;
+    EditText txtInput;
+    TextView txtContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,6 +96,29 @@ public class MainActivity extends AppCompatActivity {
         sdkVersion = (TextView) findViewById(R.id.sdkVersion);
         sdkVersion.setText("SDK " + DJISDKManager.getInstance().getSDKVersion());
 
+        //for custom data logs
+        txtContent = (TextView) findViewById(R.id.txtContent);
+        txtInput = (EditText) findViewById(R.id.txtInput);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtContent.setText(FileHelper.ReadFile(MainActivity.this));
+            }
+        });
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (FileHelper.saveToFile( txtInput.getText().toString())){
+                    Toast.makeText(MainActivity.this,"Saved to file",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Error save file!!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     /**
@@ -241,7 +272,6 @@ public class MainActivity extends AppCompatActivity {
             camera.setMode(cameraMode, new CommonCallbacks.CompletionCallback() {
                 @Override
                 public void onResult(DJIError djiError) {
-
                     if (djiError == null) {
                         showToast("Camera mode set to video");
                     } else {
